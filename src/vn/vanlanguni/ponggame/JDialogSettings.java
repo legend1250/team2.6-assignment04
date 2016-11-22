6964c49315fb3a3bfd536468d7c2e43912096b77
MinhQuan <MinhQuan@192.168.1.5>
2016-11-22 22:16:52 +0700

    requirement change color paddles

--- src/vn/vanlanguni/ponggame/JDialogSettings.java
+++ src/vn/vanlanguni/ponggame/JDialogSettings.java
@@ -11,77 +11,91 @@ import javax.swing.JDialog;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
 import javax.swing.border.EtchedBorder;
 
-public class JDialogSettings extends JDialog{
+public class JDialogSettings extends JDialog {
 	final static int WIDTH = 400, HEIGHT = 600;
-	
+
 	private Color BallColor;
 	private String namePlayer01, namePlayer02;
 	private Color BackgroundColor;
-	
-	
+	private Color PaddlesColor;
 
 	BallPanel ballPanel = new BallPanel();
-	
-	public JDialogSettings(){
+	PaddlesPanel paddlesPanel = new PaddlesPanel();
+
+	public JDialogSettings() {
 		setTitle("Config infomation");
-		setPreferredSize(new Dimension(WIDTH,HEIGHT));
-		setLocation(380,150);
+		setPreferredSize(new Dimension(WIDTH, HEIGHT));
+		setLocation(380, 150);
 		setLayout(null);
 		pack();
-		
+
 		initMenuComponent();
-		//add Panel
+		// add Panel
 		this.add(ballPanel);
+		this.add(paddlesPanel);
 		int h_ballPanel = 450;
-		ballPanel.setBounds(0,0,WIDTH,h_ballPanel);
+		ballPanel.setBounds(0, 0, WIDTH, h_ballPanel);
 		ballPanel.setVisible(false);
 		ballPanel.setBorder(new EtchedBorder());
-		
-		initComponents();		
-		
+		int h_paddlesPanel = 450;
+		paddlesPanel.setBounds(0, 0, WIDTH, h_paddlesPanel);
+		paddlesPanel.setVisible(false);
+		paddlesPanel.setBorder(new EtchedBorder());
 	}
-	
+
 	JMenuBar mnbMenuBar;
-	JMenu mnuUser, mnuBall, mnuBackground;
-	JMenuItem mniSetNameOfUser, mniSetColorOfBall;
-	private void initMenuComponent(){
-		//init menuComponent
+	JMenu mnuUser, mnuBall, mnuPaddles, mnuBackground;
+	JMenuItem mniSetNameOfUser, mniSetColorOfBall, mniSetColorOfPaddles;
+
+	private void initMenuComponent() {
+		// TODO Auto-generated method stub
+		// init menuComponent
 		mnbMenuBar = new JMenuBar();
 		mnuUser = new JMenu("User");
 		mnuBall = new JMenu("Ball");
+		mnuPaddles = new JMenu("Paddles");
 		mnuBackground = new JMenu("Background");
 		mniSetNameOfUser = new JMenuItem("Set name");
 		mniSetColorOfBall = new JMenuItem("Set color");
-		//add
+		mniSetColorOfPaddles = new JMenuItem("Set color");
+		// add
 		setJMenuBar(mnbMenuBar);
-		//add to menubar
+		// add to menubar
 		mnbMenuBar.add(mnuUser);
 		mnbMenuBar.add(mnuBall);
+		mnbMenuBar.add(mnuPaddles);
 		mnbMenuBar.add(mnuBackground);
-		//add to Menu
+		// add to Menu
 		mnuUser.add(mniSetNameOfUser);
-		
+		mnuPaddles.add(mniSetColorOfPaddles);
 		mnuBall.add(mniSetColorOfBall);
-		
+
 		ActionListener actionBall = new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if(e.getSource()== mniSetColorOfBall){
+				if (e.getSource() == mniSetColorOfBall) {
 					ballPanel.setVisible(true);
+					paddlesPanel.setVisible(false);
+				}
+				if (e.getSource() == mniSetColorOfPaddles) {
+					paddlesPanel.setVisible(true);
+					ballPanel.setVisible(false);
 				}
 			}
 		};
-		
-		//add ActionListener to JMenuItem
+		// add ActionListener to JMenuItem
+
 		mniSetColorOfBall.addActionListener(actionBall);
+		mniSetColorOfPaddles.addActionListener(actionBall);
+
 	}
-	
+
 	JButton btnSaveInfo = new JButton("Save"), btnExit = new JButton("Exit");
-	private void initComponents(){
+
+	private void initComponents() {
 		Insets isMargin = new Insets(1, 1, 1, 1);
 		add(btnSaveInfo);
 		btnSaveInfo.setFocusable(false);
@@ -91,91 +105,100 @@ public class JDialogSettings extends JDialog{
 		btnExit.setFocusable(false);
 		btnExit.setMargin(isMargin);
 		btnExit.setBounds(315, 500, 60, 30);
-		
+
 		ActionListener actionButton = new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
 				JButton btnT = (JButton) e.getSource();
-				if(btnT.equals(btnSaveInfo)){
-					//saveInfo();
-				}
-				else if(btnT.equals(btnExit)){
-					/*if(BallColor != Color.RED){
-						int result = JOptionPane.showConfirmDialog(null, "You haven't save yet! Do you want to save then Exit?");
-						if(result == JOptionPane.YES_OPTION){
-							//saveInfoThenExit();
-						}
-						else if(result == JOptionPane.NO_OPTION){
-							dispose();
-						}
-					}
-					else{
-						dispose();
-					}*/
+				if (btnT.equals(btnSaveInfo)) {
+					// saveInfo();
+				} else if (btnT.equals(btnExit)) {
+					/*
+					 * if(BallColor != Color.RED){ int result =
+					 * JOptionPane.showConfirmDialog(null,
+					 * "You haven't save yet! Do you want to save then Exit?");
+					 * if(result == JOptionPane.YES_OPTION){
+					 * //saveInfoThenExit(); } else if(result ==
+					 * JOptionPane.NO_OPTION){ dispose(); } } else{ dispose(); }
+					 */
 					dispose();
 				}
 			}
 		};
 		btnSaveInfo.addActionListener(actionButton);
 		btnExit.addActionListener(actionButton);
-		
+
 	}
-	
-	
-	private void saveInfoThenExit(){
+
+	private void saveInfoThenExit() {
 		ballPanel.setColor(ballPanel.getColor());
+		paddlesPanel.setColor(paddlesPanel.getColor());
 	}
-	
-	public BallPanel returnBallPanel(){
-		
-		if(isColorBall()){
-			ballPanel = new BallPanel(true,getBallColor());
-		}
-		else {
-			ballPanel = new BallPanel(false,getImageBallIndex());
+
+	public BallPanel returnBallPanel() {
+
+		if (isColorBall()) {
+			ballPanel = new BallPanel(true, getBallColor());
+		} else {
+			ballPanel = new BallPanel(false, getImageBallIndex());
 		}
 		return ballPanel;
-		
-		//return ballPanel;
-	}
-	
 
+		// return ballPanel;
+	}
+
+	public PaddlesPanel returnPaddlesPanel() {
+		if (isColorPaddles()) {
+			paddlesPanel = new PaddlesPanel(true, getPaddlesColor());
+		}
+
+		return paddlesPanel;
+
+		// return paddlesPanel;
+	}
 
 	public boolean isColorBall() {
-		if(ballPanel.isIsColorBall()){
+		if (ballPanel.isIsColorBall()) {
 			return true;
 		}
 		return false;
 	}
-	
-	public int getImageBallIndex(){
+
+	public boolean isColorPaddles() {
+		if (paddlesPanel.isIsColorPaddles()) {
+			return true;
+		}
+		return false;
+	}
+
+	public int getImageBallIndex() {
 		return ballPanel.getnImageIndex();
 	}
-	
+
 	public Color getBallColor() {
 		return ballPanel.getColor();
 	}
 
+	public Color getPaddlesColor() {
+		return paddlesPanel.getColor();
+	}
 
 	public String getNamePlayer01() {
 		return namePlayer01;
 	}
 
-
 	public void setNamePlayer01(String namePlayer01) {
 		this.namePlayer01 = namePlayer01;
 	}
-
 
 	public String getNamePlayer02() {
 		return namePlayer02;
 	}
 
-
 	public void setNamePlayer02(String namePlayer02) {
 		this.namePlayer02 = namePlayer02;
 	}
-	
+
 }
--- src/vn/vanlanguni/ponggame/PongPanel.java
+++ src/vn/vanlanguni/ponggame/PongPanel.java
@@ -20,7 +20,6 @@ package vn.vanlanguni.ponggame;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
-import java.awt.Image;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -34,18 +33,17 @@ import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.Timer;
-
 
 /**
  * 
  * @author Invisible Man
  *
  */
-public class PongPanel extends JPanel implements ActionListener, KeyListener, MouseMotionListener, MouseListener {
+public class PongPanel extends JPanel implements ActionListener, KeyListener,
+		MouseMotionListener, MouseListener {
 	private static final long serialVersionUID = -1097341635155021546L;
 
 	private boolean showTitleScreen = true;
@@ -68,7 +66,7 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener, Mo
 	private int ballDeltaX = -1;
 	private int ballDeltaY = 3;
 	private Color ballColor = Color.RED;
-	
+
 	/** Player 1's paddle: position and size */
 	private int playerOneX = 0;
 	private int playerOneY = 250;
@@ -80,6 +78,7 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener, Mo
 	private int playerTwoY = 250;
 	private int playerTwoWidth = 10;
 	private int playerTwoHeight = 60;
+	private Color paddlesColor = Color.RED;
 
 	/** Speed of the paddle - How fast the paddle move. */
 	private int paddleSpeed = 5;
@@ -88,15 +87,15 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener, Mo
 	private int playerOneScore;
 	private int playerTwoScore;
 
-	
 	//
 	Rectangle rect;
-	int xRect =  130, yRect = 265, wRect = 200, hRect = 55;
+	int xRect = 130, yRect = 265, wRect = 200, hRect = 55;
 	Rectangle rectSettings;
-	int xSettings = 428, ySettings = yRect, wSettings = hRect, hSettings = hRect;
-	//ImageIcon imagePlayGame, imageSettings;
+	int xSettings = 428, ySettings = yRect, wSettings = hRect,
+			hSettings = hRect;
+	// ImageIcon imagePlayGame, imageSettings;
 	BufferedImage imagePlayGame, imageSettings;
-	
+
 	/** Construct a PongPanel. */
 	public PongPanel() {
 		setBackground(backgroundColor);
@@ -106,22 +105,22 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener, Mo
 		addKeyListener(this);
 		addMouseMotionListener(this);
 		addMouseListener(this);
-		
-		//new rect
-		rect = new Rectangle(xRect,yRect,wRect,hRect);
-		rectSettings = new Rectangle(xSettings,ySettings,wSettings,hSettings);
+
+		// new rect
+		rect = new Rectangle(xRect, yRect, wRect, hRect);
+		rectSettings = new Rectangle(xSettings, ySettings, wSettings, hSettings);
 
 		try {
 			imagePlayGame = ImageIO.read(new File("images/btn_playgame.png"));
 			imageSettings = ImageIO.read(new File("images/setting.png"));
-			//Ball Image
+			// Ball Image
 			imgSoccerBall = ImageIO.read(new File(imgURL[0]));
 			imgKABall = ImageIO.read(new File(imgURL[1]));
 			imgMasterBall = ImageIO.read(new File(imgURL[2]));
 		} catch (IOException e) {
 			JOptionPane.showMessageDialog(null, e.getMessage());
 		}
-		
+
 		// call step() 60 fps
 		Timer timer = new Timer(1000 / 60, this);
 		timer.start();
@@ -145,7 +144,8 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener, Mo
 				playerOneY -= paddleSpeed;
 			}
 			// Move down if after moving paddle is not outside the screen
-			if (downPressed && playerOneY + playerOneHeight + paddleSpeed < getHeight()) {
+			if (downPressed
+					&& playerOneY + playerOneHeight + paddleSpeed < getHeight()) {
 				playerOneY += paddleSpeed;
 			}
 
@@ -155,7 +155,8 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener, Mo
 				playerTwoY -= paddleSpeed;
 			}
 			// Move down if after moving paddle is not outside the screen
-			if (sPressed && playerTwoY + playerTwoHeight + paddleSpeed < getHeight()) {
+			if (sPressed
+					&& playerTwoY + playerTwoHeight + paddleSpeed < getHeight()) {
 				playerTwoY += paddleSpeed;
 			}
 
@@ -188,7 +189,8 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener, Mo
 			// will the ball go off the left side?
 			if (nextBallLeft < playerOneRight) {
 				// is it going to miss the paddle?
-				if (nextBallTop > playerOneBottom || nextBallBottom < playerOneTop) {
+				if (nextBallTop > playerOneBottom
+						|| nextBallBottom < playerOneTop) {
 
 					playerTwoScore++;
 
@@ -201,7 +203,7 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener, Mo
 					ballY = 200;
 				} else {
 					// If the ball hitting the paddle, it will bounce back
-					
+
 					ballDeltaX *= -1;
 				}
 			}
@@ -209,7 +211,8 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener, Mo
 			// will the ball go off the right side?
 			if (nextBallRight > playerTwoLeft) {
 				// is it going to miss the paddle?
-				if (nextBallTop > playerTwoBottom || nextBallBottom < playerTwoTop) {
+				if (nextBallTop > playerTwoBottom
+						|| nextBallBottom < playerTwoTop) {
 
 					playerOneScore++;
 
@@ -222,7 +225,7 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener, Mo
 					ballY = 200;
 				} else {
 					// If the ball hitting the paddle, it will bounce back
-					
+
 					ballDeltaX *= -1;
 				}
 			}
@@ -239,7 +242,7 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener, Mo
 	/** Paint the game screen. */
 	boolean hoverPlayGame;
 	boolean hoverSettings;
-	
+
 	public void paintComponent(Graphics g) {
 
 		super.paintComponent(g);
@@ -253,22 +256,25 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener, Mo
 			g.setFont(new Font(Font.DIALOG, Font.BOLD, 36));
 			g.drawString("Pong Game", 130, 100);
 
-			
-			if(hoverPlayGame){
+			if (hoverPlayGame) {
 				g.setColor(Color.WHITE);
 				g.fillRect(xRect, yRect, wRect, hRect);
-				g.drawImage(imagePlayGame, xRect, yRect, xRect + wRect, yRect + hRect, 0, 0, 522, 186, null);
+				g.drawImage(imagePlayGame, xRect, yRect, xRect + wRect, yRect
+						+ hRect, 0, 0, 522, 186, null);
+			} else {
+				g.drawImage(imagePlayGame, xRect, yRect, xRect + wRect, yRect
+						+ hRect, 0, 0, 522, 186, null);
 			}
-			else{
-				g.drawImage(imagePlayGame, xRect, yRect, xRect + wRect, yRect + hRect, 0, 0, 522, 186, null);
+			if (hoverSettings) {
+				g.drawImage(imageSettings, xSettings, ySettings, xSettings
+						+ wSettings, ySettings + hSettings, 0, 0, 300, 300,
+						null);
+			} else {
+				g.drawImage(imageSettings, xSettings, ySettings, xSettings
+						+ wSettings, ySettings + hSettings, 0, 0, 300, 300,
+						null);
 			}
-			if(hoverSettings){
-				g.drawImage(imageSettings, xSettings, ySettings, xSettings + wSettings, ySettings + hSettings, 0, 0, 300, 300, null);
-			}
-			else{
-				g.drawImage(imageSettings, xSettings, ySettings, xSettings + wSettings, ySettings + hSettings, 0, 0, 300, 300, null);
-			}
-			
+
 		} else if (playing) {
 
 			/* Game is playing */
@@ -295,24 +301,25 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener, Mo
 																	// score
 
 			// draw the ball
-			if(isColorBall){
+			if (isColorBall) {
 				g.setColor(ballColor);
 				g.fillOval(ballX, ballY, diameter, diameter);
-			}
-			else{
-				if(ball.getnImageIndex() == 0){
-					g.drawImage(imgSoccerBall, ballX, ballY, ballX + diameter, ballY + diameter, 0, 0, 256, 256, null);
-				}
-				else if (ball.getnImageIndex()== 1){
-					g.drawImage(imgKABall, ballX, ballY, ballX + diameter, ballY + diameter, 0, 0, 333, 328, null);
-				}
-				else if (ball.getnImageIndex() == 2){
-					g.drawImage(imgMasterBall, ballX, ballY, ballX + diameter, ballY + diameter, 0, 0, 400, 400, null);
+			} else {
+				if (ball.getnImageIndex() == 0) {
+					g.drawImage(imgSoccerBall, ballX, ballY, ballX + diameter,
+							ballY + diameter, 0, 0, 256, 256, null);
+				} else if (ball.getnImageIndex() == 1) {
+					g.drawImage(imgKABall, ballX, ballY, ballX + diameter,
+							ballY + diameter, 0, 0, 333, 328, null);
+				} else if (ball.getnImageIndex() == 2) {
+					g.drawImage(imgMasterBall, ballX, ballY, ballX + diameter,
+							ballY + diameter, 0, 0, 400, 400, null);
 				}
 			}
 			// draw the paddles
-			g.setColor(Color.RED);
+			g.setColor(paddlesColor);
 			g.fillRect(playerOneX, playerOneY, playerOneWidth, playerOneHeight);
+			g.setColor(paddlesColor);
 			g.fillRect(playerTwoX, playerTwoY, playerTwoWidth, playerTwoHeight);
 		} else if (gameOver) {
 
@@ -336,17 +343,16 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener, Mo
 			g.setColor(Color.YELLOW);
 			g.setFont(new Font(Font.DIALOG, Font.BOLD, 18));
 			g.drawString("Press 'Space' to restart the game", 150, 250);
-			
-			//reset score each player
+
+			// reset score each player
 			playerOneScore = playerTwoScore = 0;
 		}
 	}
 
 	public void keyTyped(KeyEvent e) {
-	
+
 	}
 
-	
 	public void keyPressed(KeyEvent e) {
 		if (showTitleScreen) {
 			if (e.getKeyChar() == 'p') {
@@ -383,81 +389,95 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener, Mo
 		} else if (e.getKeyCode() == KeyEvent.VK_S) {
 			sPressed = false;
 		}
-		
+
 	}
-	
-	public void setBallColor(Color newColor){
+
+	public void setBallColor(Color newColor) {
 		this.ballColor = newColor;
 	}
-	
+
+	public void setPaddlesColor(Color newColor1) {
+		this.paddlesColor = newColor1;
+	}
+
 	@Override
 	public void mouseMoved(MouseEvent arg0) {
-		//System.out.println(String.format("%d %d",arg0.getX(), arg0.getY()));
-		if(showTitleScreen){
-			//rectangle playgame
-			if(rect.contains(arg0.getX(), arg0.getY())){
+		// System.out.println(String.format("%d %d",arg0.getX(), arg0.getY()));
+		if (showTitleScreen) {
+			// rectangle playgame
+			if (rect.contains(arg0.getX(), arg0.getY())) {
 				hoverPlayGame = true;
-			}
-			else{
+			} else {
 				hoverPlayGame = false;
 			}
-			
-			//rectangle settings
-			if(rectSettings.contains(arg0.getX(),arg0.getY())){
+
+			// rectangle settings
+			if (rectSettings.contains(arg0.getX(), arg0.getY())) {
 				hoverSettings = true;
-			}
-			else{
+			} else {
 				hoverSettings = false;
 			}
 		}
 	}
-	
+
 	JDialogSettings settings;
 	BallPanel ball;
+	PaddlesPanel paddles;
+
 	public void mouseClicked(MouseEvent arg0) {
-		
-		if(showTitleScreen){
-			if(hoverPlayGame){
+
+		if (showTitleScreen) {
+			if (hoverPlayGame) {
 				showTitleScreen = false;
 				playing = true;
 			}
-			if(hoverSettings){
+			if (hoverSettings) {
 				settings = new JDialogSettings();
 				settings.setModal(true);
 				settings.setVisible(true);
+
+				paddles = settings.returnPaddlesPanel();
+				if (paddles.isIsColorPaddles()) {
+					setPaddlesColor(paddles.getColor());
+					isColorPaddles = true;
+				} else {
+					isColorPaddles = false;
+				}
+
 				ball = settings.returnBallPanel();
-				if(ball.isIsColorBall()){
+				if (ball.isIsColorBall()) {
 					setBallColor(ball.getColor());
 					isColorBall = true;
-				}
-				else{
+				} else {
 					isColorBall = false;
 				}
+
 				settings.dispose();
 			}
 		}
-		
+
 	}
 
 	private boolean isColorBall;
-	
-	BufferedImage imgBallPlaying,imgSoccerBall, imgKABall, imgMasterBall;
-	String[] imgURL = {
-			"images/SoccerBall.png",
-			"images/KA_Ball.png",
-			"images/MasterBall.png"
-	};
-	
-	
-	public void mouseEntered(MouseEvent arg0) { }
+	private boolean isColorPaddles;
 
-	public void mouseExited(MouseEvent arg0) { }
+	BufferedImage imgBallPlaying, imgSoccerBall, imgKABall, imgMasterBall;
+	String[] imgURL = { "images/SoccerBall.png", "images/KA_Ball.png",
+			"images/MasterBall.png" };
 
-	public void mousePressed(MouseEvent arg0) { }
+	public void mouseEntered(MouseEvent arg0) {
+	}
 
-	public void mouseReleased(MouseEvent arg0) { }
+	public void mouseExited(MouseEvent arg0) {
+	}
 
-	public void mouseDragged(MouseEvent arg0) { }
+	public void mousePressed(MouseEvent arg0) {
+	}
 
-	
+	public void mouseReleased(MouseEvent arg0) {
+	}
+
+	public void mouseDragged(MouseEvent arg0) {
+	}
+
 }
